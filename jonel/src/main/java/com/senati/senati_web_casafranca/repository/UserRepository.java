725ced5e3bc6369f7bff3be07d4aa3422ff1a956
jonel-ascoitia <1618942@senati.pe>
2025-10-16 07:38:10 -0500

    Delete jonel directory

--- jonel/src/main/java/com/senati/senati_web_casafranca/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senati.senati_web_ascoitia.repository;
-
-import com.senati.senati_web_ascoitia.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
